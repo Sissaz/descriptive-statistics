aba0f0435bdaef37ff1ed8c9a6d653441f86ee5e
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-05-02 14:43:36 -0300

    complete

--- visao-geral.ipynb/conjuntos.py
+++ visao-geral.ipynb/conjuntos.py
@@ -5,11 +5,13 @@ c = set(lista)
 type(c)
 # set
 
+-
 
 c.add(55)
 c
 # {1, 2, 3, 4, 5, 55}
 
+-
 
 # Numeros em comum
 c1 = set([1,2,3,4])
@@ -17,14 +19,18 @@ c2 = set((1,2,88))
 c1.intersection(c2)
 # {1, 2}
 
+-
 
 # Uniao
 c1.union(c2)
 # {1, 2, 3, 4, 88}
 
+-
 
 8 in c1
 # False
+
+-
 
 2 in c1
 # True
