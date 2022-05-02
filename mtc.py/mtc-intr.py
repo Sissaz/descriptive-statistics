9692337d50e9c23f7159fb52d00da6b0fea2c57b
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-05-02 16:55:50 -0300

    complete

---
+++ mtc.py/mtc-intr.py
@@ -0,0 +1,21 @@
+# Medidas de Tendência Central
+
+# Indicam um ponto em torno do qual se concentram os dados;
+# Medidas descritivas para resumir dados;
+# Se calculadas a partir de dados populacionais, temos PARÂMETROS;
+# Se calculadas a partir de dados amostrais, temos ESTIMADORES ou ESTATÍSTICAS;
+# Estatísticas: Média, Moda, Mediana, Percentis.
+
+# Notações das Estatísticas:
+
+# Para Parâmetros:
+# Número de Elementos: N
+# Média: μ
+# Variância: σ²
+# Desvio Padrão: σ
+
+# Para Estimadores:
+# Número de Elementos: n
+# Média: x̅
+# Variância: S²
+# Desvio Padrão: S
