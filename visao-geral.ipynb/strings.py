053171cdca3e599ea1919d478f6d556dfda7f202
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-05-02 14:47:15 -0300

    complete

--- visao-geral.ipynb/strings.py
+++ visao-geral.ipynb/strings.py
@@ -4,29 +4,45 @@ s = str (77)
 s
 # 77
 
+-
+
 t = 'O Python...'
 len(t)
 # 11
 
+-
+
 t[:8]
 # O Python
+
+-
 
 t.upper()
 # O PYTHON...
 
+-
+
 t.lower()
 # o python...
 
+-
+
 t.find("Python")
 # 2
+
+-
 
 g = t.split("o")
 g
 # O Pyth, n...
 
+-
+
 s = t.split(" ")
 s
 # O, Python...
+
+-
 
 type(g)
 # list
