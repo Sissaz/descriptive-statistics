fed6fb73b451401ca9d748e82c3fa1643bf0f29d
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-05-02 17:27:37 -0300

    complete

--- mtc.py/media.py
+++ mtc.py/media.py
@@ -34,3 +34,26 @@ np.mean(dados)
 # sepal_width     3.057333
 # petal_length    3.758000
 # petal_width     1.199333
+
+-
+
+# Calculando a Mediana
+
+mediana_sl = np.median(dados['sepal_length'])
+mediana_sl
+
+# 5.8
+
+moda_sl = dados['sepal_length'].median()
+moda_sl
+
+# 5.8
+
+-
+
+# Calculando a Moda
+
+median_sl = dados['sepal_length'].mode()
+median_sl
+
+# 5.0
