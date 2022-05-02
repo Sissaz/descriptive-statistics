722707b9b8ce9c62796cd677a4dab1fe8bb1d665
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-05-02 17:22:17 -0300

    complete

---
+++ mtc.py/media.py
@@ -0,0 +1,36 @@
+# Calculando a Média
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
+import pandas as pd
+from bokeh.sampledata.iris import flowers as dados
+
+dados.shape
+
+# (150, 5)
+# 150 linhas e 5 colunas
+
+-
+
+dados.head(3)
+# Plotagem da tabela com 3 linhas e as 5 colunas
+
+-
+
+med_sl = np.mean(dados['sepal_length'])
+print(round(med_sl))
+# Imprime a média arredondada para o comprimento da Sépala
+
+-
+
+type(dados)
+# pandas.core.frame.DataFrame
+# O tipo dessa estrutura de dados é do tipo DataFrame, presente na biblioteca Pandas.
+# Com isso, consegue-se calcular a média diretamente, com:
+np.mean(dados)
+
+# sepal_length    5.843333
+# sepal_width     3.057333
+# petal_length    3.758000
+# petal_width     1.199333
