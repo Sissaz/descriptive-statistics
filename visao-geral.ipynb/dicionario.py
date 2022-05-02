872c8ad9b60111e27a51f7be15df8064b8615e08
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-05-02 14:44:09 -0300

    complete

--- visao-geral.ipynb/dicionario.py
+++ visao-geral.ipynb/dicionario.py
@@ -7,19 +7,29 @@ dicionario = {}
 type(dicionario)
 # dict
 
+-
+
 dic1 = {'A':1, 'B':2, 'C':3, 'D':4}
 dic1
 # {'A': 1, 'B': 2, 'C':3, 'D': 4}
+
+-
 
 dic2 = dict(A=1,B=2,C=77)
 dic2
 # # {'A': 1, 'B': 2, 'C':77}
 
+-
+
 len(dic2)
 # 3
 
+-
+
 dic2.keys()
 # dict_keys (['A', 'B', 'C'])
+
+-
 
 dic2.values()
 #dict_values([1, 2, 77])
